6d44cf875646a25e7ac93947075ff7880cc26331
theburningmonk <theburningmonk@gmail.com>
2017-06-18 23:59:55 +0100

    - put the cloudwatch tracking back

--- functions/system-under-test.js
+++ functions/system-under-test.js
@@ -7,13 +7,30 @@ const cloudwatch = Promise.promisifyAll(new AWS.CloudWatch());
 
 let isColdstart = false;
 
-module.exports.handler = (event, context, callback) => {
+let trackColdstart = co.wrap(function* (funcName) {
+  let req = {
+    MetricData: [
+      {
+        MetricName: `coldstart`,
+        Dimensions: [ { Name: "functionName", Value: funcName } ],
+        Timestamp: new Date(),
+        Unit: 'Count',
+        Value: 1
+      }
+    ],
+    Namespace: "theburningmonk.com"
+  };
+  yield cloudwatch.putMetricDataAsync(req);
+});
+
+module.exports.handler = co.wrap(function* (event, context, callback) {
   if (!isColdstart) {
     isColdstart = true;
     console.log("this is a coldstart");
+    yield trackColdstart(context.functionName);
 
     callback(null, { isColdstart : true });
   }
 
   callback(null, { isColdstart : false });
-};
+});
